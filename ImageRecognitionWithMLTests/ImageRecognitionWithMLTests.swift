329e135ef3f3405d2caaea050312d8075ca5569e
korkmazanil <korkmaz.anil41@gmail.com>
2024-11-20 23:30:56 +0300

    Initial Commit

---
+++ ImageRecognitionWithMLTests/ImageRecognitionWithMLTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRecognitionWithMLTests.swift
+//  ImageRecognitionWithMLTests
+//
+//  Created by ANIL KORKMAZ on 20.11.2024.
+//
+
+import Testing
+@testable import ImageRecognitionWithML
+
+struct ImageRecognitionWithMLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
